e88785b70259b812c09a8fffc5fb1ef8c07d032b
Sarthak Sharma <sarthaksharm372@gmail.com>
2025-05-01 03:25:32 +0530

    configured tw colors and fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from 'next'
+import { Gilda_Display, Inter } from 'next/font/google'
+import localFont from 'next/font/local'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -7,6 +9,42 @@ export const metadata: Metadata = {
     'A responsive and modern clone of the Attio CRM landing page, built by Sarthak Sharma as a front-end development project. Made with React and Tailwind CSS.',
 }
 
+// ! Figure out the preload warning
+const inter = Inter({
+  subsets: ['latin'],
+  // weight: ['400', '500', '600', '700'],
+  display: 'swap',
+  variable: '--font-inter',
+  fallback: ['sans-serif'],
+})
+
+const gilroy = localFont({
+  src: [
+    {
+      path: './fonts/Gilroy/Gilroy-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Gilroy/Gilroy-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Gilroy/Gilroy-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Gilroy/Gilroy-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+  variable: '--font-gilroy',
+})
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -14,7 +52,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`antialiased`}>{children}</body>
+      <body
+        className={`antialiased ${inter.variable} ${gilroy.variable} font-inter`}
+      >
+        {children}
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
     <>
-      <h1>Attio Landing Page Clone</h1>
+      <h1>Attio</h1>
+      <h2>Attio</h2>
+      <h3>Attio</h3>
+      <p className='text-2xl'>Attio Landing Page Clone</p>
+      <p className='text-xl'>Attio Landing Page Clone</p>
+      <p className='text-lg'>Attio Landing Page Clone</p>
+      <p className='text-md'>Attio Landing Page Clone</p>
+      <p className='text-base'>Attio Landing Page Clone</p>
+      <p className='text-sm'>Attio Landing Page Clone</p>
+      <p className='text-xs'>Attio Landing Page Clone</p>
     </>
   )
 }
